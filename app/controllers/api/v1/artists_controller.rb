0f03554b53f5bf4ac87edd703ecb502d29416f93
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-19 16:37:21 -0400

    Add artist create action and param handler

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -7,12 +7,19 @@ module Api
       end
 
       def create
+        artist = Artist.create(artist_params)
+        render json: artist
       end
 
       def update
       end
 
       def destroy
+      end
+
+      private
+      def artist_params
+        params.require(:data).require(:attributes).permit(:name)
       end
 
     end
