17058ce6a07f3153174d06d9a86f3f7c2e0f8f78
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-21 00:53:31 -0400

    Remove status from event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :price, :description, :image, :upcoming, :status
+  attributes :id, :name, :date, :price, :description, :image, :upcoming
   belongs_to :presenter
   has_many :artists
 end
